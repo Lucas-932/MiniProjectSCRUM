f4ffe79996a7905e70fd4ada177dbda43d658e4f
John Spruijt <johnspruijt@outlook.com>
2024-02-21 14:41:25 +0100

    Vincent Added DisplayCompass

--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@
     public static void Main()
     {
         running = true;
-
+ 
         while (running) {
             Console.WriteLine("What would you like to do (Enter a number)?");
             Console.WriteLine("1: See game stats");
             Console.WriteLine("2: Move");
             Console.WriteLine("3: Fight");
             Console.WriteLine("4: Quit");
-
+ 
             string UserAction = Console.ReadLine() ?? "";
-
+ 
             switch (UserAction)
             {
                 case "1":
@@ -30,11 +30,11 @@
                     // quit
                     break;
             }
-
+ 
             CheckWinCondition();
         }
     }
-
+ 
     // checks if the quests have the boolean Completed to true and if one quest doesn't fail Wincondition
     public static void CheckWinCondition() {
         bool questsUncleared = false;
@@ -43,42 +43,50 @@
                 questsUncleared = true;
             }
         }
-        
+       
         if (!questsUncleared) {
             running = false;
             Console.WriteLine("You have won the game!");
         }
     }
-    
+   
     // Move functionality
     public static  void Move() {
         bool moved;
         do
-        {   
+        {
             // Prints UI
             Console.WriteLine("Where would you like to go?");
             Console.WriteLine($"You are at: {player.CurrentLocation.Name}. From here you can go:");
             DisplayCompass();
             string UserAction  = (Console.ReadLine() ?? "").ToLower();
-
+ 
             // Checks for which direction and if possible, set current location to the correct location
             moved = player.Move(UserAction);
         } while (!moved);
     }
-
-    public static void DisplayCompass() {
-        // Possible direction compass here
-        Console.WriteLine("Direction Compass not yet implemented. From Home you can go (N)orth");
+ 
+    public static void DisplayCompass()
+    {
+        if (player.CurrentLocation.LocationToNorth != null)
+            Console.WriteLine("(N)orth");
+        if (player.CurrentLocation.LocationToWest != null)
+            Console.WriteLine("(W)est");
+        if (player.CurrentLocation.LocationToEast != null)
+            Console.WriteLine("(E)ast");
+        if (player.CurrentLocation.LocationToSouth != null)
+            Console.WriteLine("(S)outh");
     }
-
+ 
+ 
     // Display all current stats
     public static void DisplayStats() {
         player.DisplayStats();
-
+ 
         Console.WriteLine("\nPress ENTER to continue");
         Console.ReadLine();
     }
-
+ 
     // Checks if there is a fight and sets up the fight
     public static void Fight() {
         Monster CurrentMonster = player.CurrentLocation.MonsterLivingHere;
